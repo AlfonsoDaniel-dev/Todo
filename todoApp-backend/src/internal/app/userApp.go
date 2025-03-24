efa7aaabd3360c2c37de437a21127dc6b5e16ef8
alfonso <alfonsodancerod@gmail.com>
2025-03-23 20:02:38 -0600

    authentication plans

--- todoApp-backend/src/internal/app/userApp.go
+++ todoApp-backend/src/internal/app/userApp.go
@@ -16,6 +16,23 @@ func NewUserServices(repo domain.UserRepository) *UserServices {
 	}
 }
 
+func (S *UserServices) Login(loginDTO DTO.LoginDTO) (string, error) {
+	if loginDTO.Email == "" || loginDTO.Password == "" {
+		return "", errors.New("Username or Password is empty")
+	}
+
+	// get user Email coincides with the password
+	// 1. get password from db with the email
+	// 2. compare form password with db password
+	// 3. if it's ok generate jwt and send it through client request, if it's not ok we will return a 401 code
+	//  verify request
+	// 1. request will pass for auth middleware who will know it's a valid token
+	// 2. if pass handler will extract email from the JWT and send it to the useService who need it
+
+	return "", nil
+
+}
+
 func (S *UserServices) CreateUser(UserDTO *DTO.UserDTO) error {
 	if UserDTO.Username == "" || UserDTO.Email == "" || UserDTO.Password == "" {
 		return errors.New("username or email or password is empty")
---
+++ todoApp-backend/src/internal/infrastructure/DTO/login.go
@@ -0,0 +1,6 @@
+package DTO
+
+type LoginDTO struct {
+	Email    string `json:"username"`
+	Password string `json:"password"`
+}
