9393a826337fee772581495bd3a410945dcccdb6
alfonso <alfonso.dancerod@gmail.com>
2025-03-24 14:34:58 -0600

    reestructuracion y añadido de multiples paginas

--- todoApp-frontend/src/App.tsx
+++ todoApp-frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home/components/home'
-import './App.css'
+import Home from './pages/home/home.tsx'
 
 function App() {
 
--- todoApp-frontend/src/pages/home/home.tsx
+++ todoApp-frontend/src/pages/home/home.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
 
+import styles from "./styles/home.module.css"
+
 const Home = () => {
 
     const [count, setCount] = useState<number>(0)
 
     return (
-        <>
+        <div className={styles.home}>
+
             <div>This is the Home page</div>
 
             <div>
                 <button onClick={() => setCount(count + 1)}>Count: {count}</button>
             </div>
-        </>
+
+        </div>
     )
 
 
